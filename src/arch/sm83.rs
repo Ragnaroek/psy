6a9acd66db92df13a0216b785a6467543bd3bc2d
Michael Bohn <michael.bohn@freiheit.com>
2025-08-31 21:13:53 +0200

    basic structure and first disassemble

---
+++ src/arch/sm83.rs
@@ -0,0 +1,27 @@
+#[derive(Debug)]
+pub enum Sm83Mnemonic {
+    NOP,
+    JP,
+}
+
+pub struct Sm83Instr {
+    pub mnemonic: Sm83Mnemonic,
+    pub immediate_args: Vec<String>,
+    pub stream_args: usize,
+}
+
+pub fn decode(op: u8) -> Result<Sm83Instr, String> {
+    match op {
+        0x00 => Ok(Sm83Instr {
+            mnemonic: Sm83Mnemonic::NOP,
+            immediate_args: Vec::with_capacity(0),
+            stream_args: 0,
+        }),
+        0xC3 => Ok(Sm83Instr {
+            mnemonic: Sm83Mnemonic::JP,
+            immediate_args: Vec::with_capacity(0),
+            stream_args: 2,
+        }),
+        _ => Err(format!("unknow instruction: {:X}", op).to_string()),
+    }
+}
