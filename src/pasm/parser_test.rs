701e1fe511dd809fdc38d8088914f94843bda5bd
Michael Bohn <spaceodyssey@gmx.de>
2025-09-28 14:49:43 +0200

    interpret def-section

--- src/pasm/parser_test.rs
+++ src/pasm/parser_test.rs
@@ -7,6 +7,6 @@ fn test_parse_test_file() -> Result<(), String> {
     let mut f = File::open("testdata/pasm/test.asm").map_err(|e| e.to_string())?;
     let tl = parse_file(&mut f)?;
 
-    assert_eq!(tl.forms.len(), 14);
+    assert_eq!(tl.forms.len(), 13);
     Ok(())
 }
